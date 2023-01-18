25987dc8874ecaaec0ad37a84a5f249d826aeb52
Klaas Gunst <klaasgunst@gmail.com>
2023-01-18 17:42:20 +0100

    Does tddmrg fail for (quasi)-degenerate subspaces?

--- mps_fqe/wavefunction.py
+++ mps_fqe/wavefunction.py
@@ -5,6 +5,8 @@ from typing import List, Union, Optional, Tuple
 import fqe
 from pyblock3.algebra.flat import FlatSparseTensor
 from pyblock3.algebra.mps import MPS
+from pyblock3.algebra.mpe import MPE
+from pyblock3.algebra.integrate import rk4_apply
 from pyblock3.algebra.symmetry import SZ
 
 
@@ -34,9 +36,9 @@ class MPSWavefunction(MPS):
             physical_q_labels = list(map(SZ.to_flat, [SZ(0, 0, 0), SZ(1, 1, 0), SZ(1, -1, 0), SZ(2, 0, 0)]))
 
             shapes = np.ones((n_blocks, ndim + 2), dtype=np.uint32)
-            data = np.concatenate([fqe_wfn.get_coeff(sector).flatten() for sector in sectors])
 
             q_labels = []
+            data_array = []
             for sector in sectors:
                 alpha_str = fqe_wfn.sector(sector).get_fcigraph().string_alpha_all().astype(int)
                 alpha_occ = np.array([tuple(reversed(bin(1 << ndim | x)[3:])) for x in alpha_str], dtype=np.uint32)
@@ -44,11 +46,19 @@ class MPSWavefunction(MPS):
                 beta_occ = np.array([tuple(reversed(bin(1 << ndim | x)[3:])) for x in beta_str], dtype=np.uint32)
                 q_labels_temp = alpha_occ[:, None, :] + 2 * beta_occ[None, :, :]
                 q_label = np.array([physical_q_labels[i] for i in q_labels_temp.flatten()]).reshape(-1, ndim)
+
+                cumsum_alpha_occ = np.cumsum(alpha_occ, axis=1)
+                n_alpha, n_beta = sum(alpha_occ[0]), sum(beta_occ[0])
+                swaps = np.sum(cumsum_alpha_occ[:, None, :] * beta_occ[None, :, :], axis=2) + (n_alpha * n_beta)
+                fermionic_sign = np.where(swaps % 2 == 0, 1.0, -1.0)
+                data_array.append((fermionic_sign * fqe_wfn.get_coeff(sector)).flatten())
+
                 vacuum = np.ones(len(q_label)) * SZ(0, 0, 0).to_flat()
                 target = np.ones(len(q_label)) * SZ(sector[0], sector[1], 0).to_flat()
                 q_labels.append(np.hstack((vacuum[:, None], q_label, target[:, None])))
 
             q_labels_arr = np.vstack(q_labels)
+            data = np.concatenate(data_array)
             return FlatSparseTensor(q_labels_arr, shapes, data)
 
         fci_tensor = get_fci_FlatSparseTensor(sectors)
@@ -56,7 +66,7 @@ class MPSWavefunction(MPS):
 
     def to_fqe_wavefunction(self, broken: Optional[Union[List[str], str]] = None) -> fqe.Wavefunction:
         """This is quite a memory intensive function. Will fail for all but the smallest MPS's."""
-        fci_tensor = functools.reduce(lambda x, y: np.tensordot(x, y, axes=1), self.tensors)
+        fci_tensor = self.get_FCITensor()
         norb = fci_tensor.ndim - 2
         sectors = tuple(map(SZ.from_flat, set(fci_tensor.q_labels[:, -1])))
 
@@ -101,9 +111,37 @@ class MPSWavefunction(MPS):
     def transform(self):
         pass
 
-    def time_evolve(self):
-        pass
+    def tddmrg(self, time: float, hamiltonian: MPS, bdim: int = 100, steps: Optional[int] = None):
+        steps = 1 if steps is None else steps
+        dt = time / steps
+        mps = self.copy()
+        mpe = MPE(mps, hamiltonian, mps)
+        mpe.tddmrg(bdims=[bdim], dt=-dt * 1j, iprint=0, n_sweeps=steps, normalize=False)
+        return type(self)(tensors=mps.tensors)
 
-    def expectationValue(self, MPO: MPS) -> float:
-        return np.conj(self) @ MPO @ self
-        pass
+    def rk4_apply(
+        self, time: float, hamiltonian: MPS, bdim: int = 100, steps: Optional[int] = None
+    ) -> "MPSWavefunction":
+        steps = 1 if steps is None else steps
+        dt = time / steps
+        mps = self.copy()
+        mps.opts = dict(max_bond_dim=bdim)
+        for ii in range(steps):
+            mps = rk4_apply((-dt * 1j) * hamiltonian, mps)
+        return type(self)(tensors=mps.tensors)
+
+    def time_evolve(
+        self, time: float, hamiltonian: MPS, bdim: int = 100, steps: Optional[int] = None, method: str = "tddmrg"
+    ) -> "MPSWavefunction":
+        if method.lower() == "tddmrg":
+            return self.tddmrg(time, hamiltonian, bdim, steps)
+        elif method.lower() == "rk4":
+            return self.rk4_apply(time, hamiltonian, bdim, steps)
+        else:
+            raise ValueError(f"method needs to be 'tddmrg' or 'rk4', '{method}' given")
+
+    def expectationValue(self, hamiltonian: MPS) -> float:
+        return MPE(self, hamiltonian, self)[0:2].expectation
+
+    def get_FCITensor(self) -> FlatSparseTensor:
+        return functools.reduce(lambda x, y: np.tensordot(x, y, axes=1), self.tensors)
--- tests/test_H_ring.py
+++ tests/test_H_ring.py
@@ -30,7 +30,7 @@ def generate_H_ring_data(molecule):
     return molecule
 
 
-@pytest.mark.parametrize("amount_H", list(range(2, 7)))
+@pytest.mark.parametrize("amount_H", list(range(2, 8)))
 def test_H_ring_evolve(amount_H):
     molecule = get_H_ring_data(amount_H)
 
@@ -46,8 +46,11 @@ def test_H_ring_evolve(amount_H):
     hamiltonian = fqe.get_restricted_hamiltonian((h1, numpy.einsum("ijlk", -0.5 * h2)), e_0=molecule.nuclear_repulsion)
     assert np.isclose(molecule.hf_energy, fqe_wf.expectationValue(hamiltonian))
 
-    time = 0.1
-    evolved = fqe_wf.time_evolve(time, hamiltonian)
+    dt = 0.1
+    steps = 10
+    evolved = fqe_wf
+    for i in range(steps):
+        evolved = evolved.time_evolve(dt, hamiltonian)
     assert np.isclose(molecule.hf_energy, evolved.expectationValue(hamiltonian))
 
     MPO = Hamiltonian(
@@ -68,3 +71,15 @@ def test_H_ring_evolve(amount_H):
 
     mps_evolved = MPSWavefunction.from_fqe_wavefunction(evolved)
     assert np.isclose(molecule.hf_energy, mps_evolved.expectationValue(MPO))
+
+    mps_evolved_2 = mps.time_evolve(dt * steps, MPO, bdim=100, steps=steps, method='rk4')
+    mps_evolved_2 /= mps_evolved_2.norm()
+
+    assert np.isclose(molecule.hf_energy, mps_evolved_2.expectationValue(MPO))
+    assert np.isclose(np.abs(mps_evolved_2.conj() @ mps_evolved), 1.0)
+
+    mps_evolved_3 = mps.time_evolve(dt * steps, MPO, bdim=100, steps=steps)
+    mps_evolved_3 /= mps_evolved_3.norm()
+
+    assert np.isclose(molecule.hf_energy, mps_evolved_3.expectationValue(MPO))
+    assert np.isclose(np.abs(mps_evolved_3.conj() @ mps_evolved), 1.0)
