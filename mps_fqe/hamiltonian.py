0da5495c24b972641f93cf252479034bb665d7f9
Justin Provazza <provazza@qsimulate.com>
2023-01-30 19:55:14 +0000

    add diagonal coulomb Hamiltonian

--- mps_fqe/hamiltonian.py
+++ mps_fqe/hamiltonian.py
@@ -13,22 +13,22 @@ from pyblock3.algebra.mps import MPS
 
 class MPOHamiltonian(Hamiltonian):
     @classmethod
-    def from_fqe_specific(cls, fqe_wf: FqeWavefunction,
+    def from_fqe_specific(cls, fqe_wfn: FqeWavefunction,
                           fqe_ham: FqeHamiltonian,
                           pg="c1",
                           flat=False) -> "MPS":
-        if not fqe_wf.conserve_number():
+        if not fqe_wfn.conserve_number():
             raise TypeError('Wavefunction does not conserve number of particles.')
-        if not fqe_wf.conserve_spin():
+        if not fqe_wfn.conserve_spin():
             raise TypeError('Wavefunction does not conserve spin.')
         #RestrictedHamiltonian
         if isinstance(fqe_ham, restricted_hamiltonian.RestrictedHamiltonian):
             h1e = fqe_ham.tensors()[0]
             g2e = numpy.einsum("ikjl", -2*fqe_ham.tensors()[1])
             fd = FCIDUMP(pg=pg,
-                         n_sites=fqe_wf.norb(),
-                         n_elec=fqe_wf._conserved['n'],
-                         twos=fqe_wf._conserved['s_z'],
+                         n_sites=fqe_wfn.norb(),
+                         n_elec=fqe_wfn._conserved['n'],
+                         twos=fqe_wfn._conserved['s_z'],
                          h1e=h1e,
                          g2e=g2e,
                          const_e=fqe_ham.e_0()
@@ -38,23 +38,25 @@ class MPOHamiltonian(Hamiltonian):
             raise TypeError('Have not implemented this type of Hamiltonian yet.')
 
     @classmethod
-    def from_fqe_hamiltonian(cls, fqe_wf: FqeWavefunction,
+    def from_fqe_hamiltonian(cls, fqe_wfn: FqeWavefunction,
                              fqe_ham: FqeHamiltonian,
                              pg="c1",
                              flat=False,
                              cutoff=1E-9) -> "MPS":
-        if not fqe_wf.conserve_number():
+        if not fqe_wfn.conserve_number():
             raise TypeError('Wavefunction does not conserve number of particles.')
-        if not fqe_wf.conserve_spin():
+        if not fqe_wfn.conserve_spin():
             raise TypeError('Wavefunction does not conserve spin.')
         fd = FCIDUMP(pg=pg,
-                     n_sites=fqe_wf.norb(),
-                     n_elec=fqe_wf._conserved['n'],
-                     twos=fqe_wf._conserved['s_z'],
+                     n_sites=fqe_wfn.norb(),
+                     n_elec=fqe_wfn._conserved['n'],
+                     twos=fqe_wfn._conserved['s_z'],
                      const_e=fqe_ham.e_0())
-        #RestrictedHamiltonian
+        # If RestrictedHamiltonian
         if isinstance(fqe_ham, restricted_hamiltonian.RestrictedHamiltonian):
             return _get_restricted_ham_mpo(fqe_ham, fd, flat=flat)
+        elif isinstance(fqe_ham, diagonal_coulomb.DiagonalCoulomb):
+            return _get_diagonal_coulomb_mpo(fqe_ham, fd, flat=flat)
 
 
 def _get_restricted_ham_mpo(fqe_ham, fd, flat):
@@ -62,7 +64,7 @@ def _get_restricted_ham_mpo(fqe_ham, fd, flat):
     hamil = Hamiltonian(fd, flat=flat)
     def generate_terms(n_sites, c, d):
         t = fqe_ham.tensors()[0]
-        v = numpy.einsum("ikjl", -2*fqe_ham.tensors()[1])
+        v = numpy.einsum("ikjl", -1*fqe_ham.tensors()[1])
         for isite in range(0, n_sites):
             for jsite in range(0, n_sites):
                 for ispin in [0, 1]:
@@ -73,8 +75,21 @@ def _get_restricted_ham_mpo(fqe_ham, fd, flat):
                     for lsite in range(0, n_sites):
                         for ijspin in [0, 1]:
                             for klspin in [0, 1]:
-                                yield 0.5*v[isite, jsite, ksite, lsite] \
+                                yield v[isite, jsite, ksite, lsite] \
                                     * (c[isite, ijspin] * c[ksite, klspin]
                                        * d[lsite, klspin] * d[jsite, ijspin])
+    return hamil.build_mpo(generate_terms, const=fqe_ham.e_0(), cutoff=0).to_sparse()
 
+def _get_diagonal_coulomb_mpo(fqe_ham, fd, flat):
+    #generate the diagonal coulomb MPO
+    hamil = Hamiltonian(fd, flat=flat)
+    def generate_terms(n_sites, c, d):
+        v = -1 * fqe_ham._tensor[2]
+        for isite in range(0, n_sites):
+            for jsite in range(0, n_sites):
+                for ispin in [0, 1]:
+                    for jspin in [0, 1]:
+                        yield v[isite, jsite] \
+                            * (c[isite, ispin] * c[jsite, jspin]
+                               * d[isite, ispin] * d[jsite, jspin])
     return hamil.build_mpo(generate_terms, const=fqe_ham.e_0(), cutoff=0).to_sparse()
