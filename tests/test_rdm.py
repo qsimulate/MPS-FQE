0143ff7e8e6f0682dfef3235ed84e948da1e8059
Justin Provazza <provazza@qsimulate.com>
2023-05-03 19:21:21 +0000

    test for block2 rdm

--- tests/test_rdm.py
+++ tests/test_rdm.py
@@ -1,11 +1,10 @@
+import os
 import numpy
-from numpy import einsum
+import tempfile
 
 import pytest
 import fqe
-from openfermion import FermionOperator
 from mps_fqe.wavefunction import MPSWavefunction
-from mps_fqe.hamiltonian import mpo_from_fqe_hamiltonian
 
 
 @pytest.mark.parametrize("n_electrons,sz,n_orbitals", [(2, 2, 4),
@@ -43,3 +42,31 @@ def test_rdm2(n_electrons, sz, n_orbitals):
     assert numpy.isclose(mps.rdm("2^ 0^ 0 2"),
                          fqe_wfn.rdm("2^ 0^ 0 2"),
                          atol=1E-12)
+
+
+@pytest.mark.parametrize("n_electrons,sz,n_orbitals", [(2, 2, 4),
+                                                       (6, -2, 4),
+                                                       (5, 1, 4),
+                                                       (6, 2, 4),
+                                                       (4, 0, 6)])
+def test_block2_rdm1(n_electrons, sz, n_orbitals):
+    from pyblock3.block2.io import MPSTools
+    from pyblock2.driver.core import DMRGDriver, SymmetryTypes
+    fqe_wfn = fqe.Wavefunction([[n_electrons, sz, n_orbitals]])
+    fqe_wfn.set_wfn(strategy='random')
+    mps = MPSWavefunction.from_fqe_wavefunction(fqe_wfn=fqe_wfn)
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        os.environ['TMPDIR'] = str(temp_dir)
+        driver = DMRGDriver(scratch=os.environ['TMPDIR'],
+                            symm_type=SymmetryTypes.SZ,
+                            n_threads=1)
+        driver.initialize_system(n_sites=mps.n_sites,
+                                 orb_sym=[0]*mps.n_sites)
+        b2mps = MPSTools.to_block2(mps, save_dir=driver.scratch)
+        rdm1 = numpy.sum(driver.get_1pdm(b2mps), axis=0)
+
+    print(len(rdm1[:, 0]), len(rdm1[0, :]))
+    assert numpy.allclose(rdm1,
+                          fqe_wfn.rdm("i^ j"),
+                          atol=1E-12)
