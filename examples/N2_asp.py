3eef9246f9803683a7bd8769a312cf241dab0774
Alec White <whiteaf862@gmail.com>
2024-02-01 19:16:50 +0000

    Cleanup imaginary time example, add adiabatic state prep example

--- examples/N2_asp.py
+++ examples/N2_asp.py
@@ -4,7 +4,6 @@ import numpy
 from pyscf import ao2mo, gto, scf
 import fqe
 from mps_fqe.wavefunction import MPSWavefunction, get_hf_mps
-from mps_fqe.hamiltonian import mpo_from_fqe_hamiltonian
 
 
 def N2_from_pyscf(r, basis):
@@ -29,10 +28,19 @@ def _(bra, ket):
     return numpy.dot(numpy.conj(bra), ket)
 
 
-def imag(hamil, wfn, ns, dt):
+def asp(h1e, g2e, fock, wfn, ns, dt):
+    """Adiabatic state preparation with linear schedule."""
+    hamil = fqe.get_restricted_hamiltonian(
+        (h1e, numpy.einsum("ikjl", -0.5 * g2e)), e_0=e0)
     energies = []
+    max_time = ns*dt
     for i in range(ns):
-        wfn = wfn.time_evolve(-1j * dt, hamil)
+        t = (i + 1)*dt
+        c1 = t/max_time
+        c2 = (max_time - t)/max_time
+        hs = fqe.get_restricted_hamiltonian(
+            (c1*h1e + c2*fock, numpy.einsum("ikjl", -c1*0.5 * g2e)), e_0=e0)
+        wfn = wfn.time_evolve(dt, hs)
         norm = numpy.sqrt(vdot(wfn, wfn))
         wfn.scale(1./norm)
         energies.append(wfn.expectationValue(hamil).real)
@@ -41,11 +49,11 @@ def imag(hamil, wfn, ns, dt):
 
 if __name__ == '__main__':
     bond_dist = 1.5
-    nsteps = 50
+    nsteps = 100
     stepsize = 0.1
-    bdim = 50
+    bdim = 100
     basis = 'sto-6g'
-    outfile = "N2_imag.dat"
+    outfile = "N2_asp.dat"
     mf = N2_from_pyscf(bond_dist, basis)
 
     nele = mf.mol.nelectron
@@ -53,6 +61,7 @@ if __name__ == '__main__':
     e0 = mf.mol.energy_nuc()
 
     norb = mf.mo_coeff.shape[1]
+    fock = mf.mo_coeff.T @ mf.get_fock() @ mf.mo_coeff
     h1e = mf.mo_coeff.T @ mf.get_hcore() @ mf.mo_coeff
     g2e = ao2mo.restore(1, ao2mo.kernel(mf.mol, mf.mo_coeff),
                         norb)
@@ -60,20 +69,19 @@ if __name__ == '__main__':
         (h1e, numpy.einsum("ikjl", -0.5 * g2e)), e_0=e0)
     print(f"Problem size (nele, norb): ({nele}, {norb})")
 
-    # Use FQE to do the imaginary time evolution (exact)
+    # Use FQE to do the adiabatic state prep (exact evolution)
     init_wf = fqe.Wavefunction([[nele, 0, norb]])
     init_wf.set_wfn(strategy='hartree-fock')  # HF initial state
-    out_fqe = imag(hamil, init_wf, nsteps, stepsize)
+    out_fqe = asp(h1e, g2e, fock, init_wf, nsteps, stepsize)
 
-    # Use MPS-FQE to do the imaginary time evolution (truncated bond dimension)
-    mpo = mpo_from_fqe_hamiltonian(hamil)
+    # Use MPS-FQE to do the adiabatic state prep (truncated bond dimension)
     mps = get_hf_mps(nele, sz, norb, bdim, cutoff=1E-12, full=True)
-    out_mps = imag(mpo, mps, nsteps, stepsize)
+    out_mps = asp(h1e, g2e, fock, mps, nsteps, stepsize)
 
     with open(outfile, 'w') as f:
         f.write(f"# System: N2, {basis}, R = {bond_dist}\n")
         f.write(f"# t, E(exact),  E(bdim={bdim})\n")
         t = stepsize
         for e_fqe, e_mps in zip(out_fqe, out_mps):
-            f.write(f"{t:4.2f} {e_fqe:10.5f} {e_mps:10.5f}\n")
+            f.write(f"{t:5.2f} {e_fqe:10.5f} {e_mps:10.5f}\n")
             t += stepsize
